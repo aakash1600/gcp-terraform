5066d1e5e80cbce47489c460e61ead1432910fda
Sai Aakash <saiaakash@Mac-653.lan>
2025-11-29 19:05:45 -0600

    Initial Code

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 7.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "sinuous-axiom-479715-g7"
+  region  = "us-central1"
+}
+
+
+
+resource "google_storage_bucket" "my-bucket" {
+  name          = "tt-githubdemo-bucket01"
+  location      = "US"
+  force_destroy = true
+  public_access_prevention = "enforced"
+}
